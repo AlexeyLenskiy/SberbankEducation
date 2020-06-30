0c83de0d36511aa36438fd342b489ba12a95fcf6
Alexey <alexeylenskiy85@gmail.com>
2020-06-30 23:40:27 +0700

    1

--- venv/Filemanage/Datamanage1/Datamanage1.py
+++ venv/Filemanage/Datamanage1/Datamanage1.py
@@ -13,39 +13,23 @@ def inputs(x):
     return (inputs)
 
 #create list of names
-count = []
+list_of_names = []
 for x in range(1, (len(data_1))):
-    count += [inputs(x)]
-    list_of_names = count
+    list_of_names += [inputs(x)]
+print(list_of_names)
 
+#email generator
+def email_gen(list_of_names):
+    emails = []
+    for i in list_of_names:
+        letter = 1
+        while i[1] + '.' + i[0][0:letter] + '@company.io' in emails:
+            letter+=1
+        emails.append(i[1] + '.' + i[0][0:letter] + '@company.io')
+    return emails
 
-#create data array
-#data_array = []
-#for x in range(len(data_1)):
-    #data_array = name(x)
-    #print(data_array)
+print(email_gen(list_of_names))
 
-
-
-
-#create list of names
-    #list_of_names = [[data_array[0]] + [data_array[1]]]
-    #print(list_of_names)
-
-
-#create email generator
-    def email_gen(list_of_names):
-        emails = []
-        for i in list_of_names:
-            letter = 1
-            while i[1] + '.' + i[0][0:letter] + '@company.io' in emails:
-                letter += 1
-            emails.append(i[1] + '.' + i[0][0:letter] + '@company.io')
-        return emails
-
-
-    print(email_gen(list_of_names))
-
-    openfile.close()
+openfile.close()
 
 
